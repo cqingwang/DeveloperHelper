bbf6a944052f3bc170c3881eb65b9ed4f65d42ba
Chan <chan@worker.lan>
2023-01-31 13:21:41 +0800

    update

--- xposedmodule/src/main/java/com/wrbug/developerhelper/xposed/dumpdex/PackerInfo.kt
+++ xposedmodule/src/main/java/com/wrbug/developerhelper/xposed/dumpdex/PackerInfo.kt
@@ -19,58 +19,38 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
  */
 object PackerInfo {
 
-    private val sPackageName = ArrayList<String>()
-    private val sTypeMap = HashMap<String, Type>()
+    private val classesMap = ArrayList<String>()
+    private val typesMap = HashMap<String, Type>()
 
-
-    /**
-     * 加固应用包含的包名，如果无法脱壳，请将application的包名，加到相应数组
-     */
-
-    /**
-     * 360加固
-     */
-    private val QI_HOO = arrayOf("com.stub.StubApp")
-    /**
-     * 爱加密
-     */
-    private val AI_JIA_MI = arrayOf("s.h.e.l.l.S")
-    /**
-     * 梆梆加固
-     */
-    private val BANG_BANG = arrayOf("com.secneo.apkwrapper.ApplicationWrapper")
-    /**
-     * 腾讯加固
-     */
-    private val TENCENT = arrayOf("com.tencent.StubShell.TxAppEntry")
-    /**
-     * 百度加固
-     */
-    private val BAI_DU = arrayOf("com.baidu.protect.StubApplication")
+    //加固应用包含的包名，如果无法脱壳，请将application的包名，加到相应数组
+    private val QI_HOO = arrayOf("com.stub.StubApp") //360加固
+    private val AI_JIA_MI = arrayOf("s.h.e.l.l.S") //爱加密
+    private val BANG_BANG = arrayOf("com.secneo.apkwrapper.ApplicationWrapper") //梆梆加固
+    private val TENCENT = arrayOf("com.tencent.StubShell.TxAppEntry") //腾讯加固
+    private val BAI_DU = arrayOf("com.baidu.protect.StubApplication") //百度加固
+    private val Ding_Xian = arrayOf("com.security.shell.AppStub1") //顶象加固
 
 
     init {
-        sPackageName.addAll(Arrays.asList(*QI_HOO))
-        sPackageName.addAll(Arrays.asList(*AI_JIA_MI))
-        sPackageName.addAll(Arrays.asList(*BANG_BANG))
-        sPackageName.addAll(Arrays.asList(*TENCENT))
-        sPackageName.addAll(Arrays.asList(*BAI_DU))
+        classesMap.addAll(Arrays.asList(*QI_HOO))
+        classesMap.addAll(Arrays.asList(*AI_JIA_MI))
+        classesMap.addAll(Arrays.asList(*BANG_BANG))
+        classesMap.addAll(Arrays.asList(*TENCENT))
+        classesMap.addAll(Arrays.asList(*BAI_DU))
+        classesMap.addAll(Arrays.asList(*Ding_Xian))
 
-        for (s in QI_HOO) {
-            sTypeMap[s] = Type.QI_HOO
-        }
-        for (s in AI_JIA_MI) {
-            sTypeMap[s] = Type.AI_JIA_MI
-        }
-        for (s in BANG_BANG) {
-            sTypeMap[s] = Type.BANG_BANG
-        }
-        for (s in TENCENT) {
-            sTypeMap[s] = Type.TENCENT
-        }
-        for (s in BAI_DU) {
-            sTypeMap[s] = Type.BAI_DU
-        }
+
+        for (s in QI_HOO) typesMap[s] = Type.QI_HOO
+
+        for (s in AI_JIA_MI) typesMap[s] = Type.AI_JIA_MI
+
+        for (s in BANG_BANG) typesMap[s] = Type.BANG_BANG
+
+        for (s in TENCENT) typesMap[s] = Type.TENCENT
+
+        for (s in BAI_DU) typesMap[s] = Type.BAI_DU
+
+        for (s in Ding_Xian) typesMap[s] = Type.Ding_Xian
 
     }
 
@@ -79,7 +59,7 @@ object PackerInfo {
     }
 
     fun find(lpparam: XC_LoadPackage.LoadPackageParam): Type? {
-        for (s in sPackageName) {
+        for (s in classesMap) {
             val clazz = XposedHelpers.findClassIfExists(s, lpparam.classLoader)
             if (clazz != null) {
                 log("find class:$s")
@@ -94,7 +74,7 @@ object PackerInfo {
 
 
     private fun getType(packageName: String): Type? {
-        return sTypeMap[packageName]
+        return typesMap[packageName]
     }
 
     enum class Type private constructor(enforceName: String) {
@@ -103,7 +83,8 @@ object PackerInfo {
         AI_JIA_MI("爱加密"),
         BANG_BANG("梆梆加固"),
         TENCENT("腾讯加固"),
-        BAI_DU("百度加固");
+        BAI_DU("百度加固"),
+        Ding_Xian("顶象加固");
 
         var application: String = ""
 
